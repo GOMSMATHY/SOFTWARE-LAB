9a4de43e08ddf139a0b49073cd44a00dd21da05a
GOMSMATHY <32321699+GOMSMATHY@users.noreply.github.com>
2017-10-03 11:48:59 +0530

    j3.java created

---
+++ j3.java
@@ -0,0 +1,18 @@
+import java.util.*; 
+ 
+class j3
+{	
+	public static void main(String args[]) 
+	{ 
+		int a; 
+
+		//declare object of Scanner Class
+		Scanner buf=new Scanner(System.in); 
+		
+		System.out.print("Enter value of a :"); 
+		/*nextInt() method of Scanner class*/
+		a=buf.nextInt(); 
+		
+		System.out.println("Value of a:" +a); 
+	} 
+}
